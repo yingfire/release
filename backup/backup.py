d62412398424b8bce13b441ef1648c0cf41d9274
666666 fire <666666 fire>
2017-04-12 21:34:00 +0800

    并没有什么想改的

--- backup/backup.py
+++ backup/backup.py
@@ -8,11 +8,11 @@ def select_release_packages():
     #判断上传目录是否存在,不存在则创建
     if not os.path.exists(source_dir):
         os.mkdir(source_dir)
-    pachage_name = os.listdir(source_dir)
+    package_name = os.listdir(source_dir)
     print ("获取发布文件名")
-    type(pachage_name)
-    print (pachage_name)
-    return pachage_name
+    print (type(package_name))
+    print (package_name)
+    return package_name
 def backup_release_dir():
     pass
 
