1c95b78f95adceec3068f20f0f9415d3cbeae936
yingfire <fire_666666@163.com>
2017-04-12 18:00:02 +0800

    添加zip类

--- backup/backup.py
+++ backup/backup.py
@@ -1,14 +1,23 @@
+#coding:utf-8
 from conf import Config as C
-import os
-def judge_source_dir():
+import os, zipfile
+
+#获取发布文件名称
+def select_release_packages():
     source_dir = "d:/source"
-    if os.path.exists(source_dir):
-        print ("source ok")
-    else:
+    #判断上传目录是否存在,不存在则创建
+    if not os.path.exists(source_dir):
         os.mkdir(source_dir)
+    pachage_name = os.listdir(source_dir)
+    print ("获取发布文件名")
+    type(pachage_name)
+    print (pachage_name)
+    return pachage_name
+def backup_release_dir():
+    pass
 
 def main():
-    judge_source_dir()
+    select_release_packages()
 
 if __name__ == '__main__':
     main()
